7096d196b5e8e14fafc1dcdbeb9dec462fd7b235
Hamzat <hamti2018@gmail.com>
2022-10-31 01:18:30 +0300

    new rewads db model

--- config-example.js
+++ config-example.js
@@ -1,26 +1,24 @@
 module.exports = {
   NODE_RPC: 'http://127.0.0.1:8732/',
   MONGO_URL: 'mongodb://localhost:27017/dbname',
-  START_INDEXING_LEVEL: 200160, // Level a baker has started
+  START_INDEXING_LEVEL: 1085761, // Level a baker has started
   BAKER_LIST: [
     'address'
   ],
   PAYMENT_SCRIPT: {
     ENABLED_AUTOPAYMENT: true, // You need to make payments manually if this option is disabled.
     AUTOPAYMENT_LEVEL: 10, // 5 is a minimal level
-    PAYMENT_FEE: 1, // fee 
     CYCLE_MAKE_AUTOPAYMENT: 5, // 0 is minimal
     BAKER_PRIVATE_KEYS: [
       'privatekey'
     ],
-    MIN_PAYMENT_AMOUNT: 0.1, // Minimal reward to address in PLEX
-    DEFAULT_BAKER_COMMISSION: 0.1, // 1 = 100%, 0.1 = 10%
-    BAKERS_COMMISSIONS: { // If you need to set a difference comissions from each bakers.
-      address1: 0.15,
-      address2: 0.1
-    },
-    ADDRESSES_COMMISSIONS: { // If you need to set a difference comissions to each addresses.
-      address3: 0
+    MIN_PAYMENT_AMOUNT: 0.01, // Minimal reward to address in PLEX
+    PAYMENT_FEE: 0.1, // fee mine
+    DEFAULT_BAKER_COMMISSION: 0.07, // 1 = 100%, 0.1 = 10%
+    BAKERS_COMMISSIONS: {},
+    ADDRESSES_COMMISSIONS: {
+      address1: 1,
+      address2: 1
     },
     MAX_COUNT_OPERATIONS_IN_ONE_BLOCK: 199 // The maximum number of operations per block (1 - 199)
   }
---
+++ models/rewardNew.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+// const constants = require('../constants')
+const { Schema } = mongoose
+
+const RewardNew = new Schema({
+  from: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true
+  },
+  to: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true
+  },
+  cycle: {
+    type: Number,
+    required: true,
+    index: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  paymentOperationHash: {
+    type: String,
+    trim: true,
+    index: true
+  }
+}, {
+  timestamps: true,
+  autoIndex: true
+})
+
+module.exports = () => {
+  return mongoose.model('RewardNew', RewardNew)
+}
